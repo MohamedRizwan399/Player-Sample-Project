98d88022cbcfabe434974f515045924890e35d7a
Mohamed Rizwan <mohamedrizwan399@gmail.com>
2024-11-17 07:14:59 +0530

    #TRAPWLS-19 code optimized

--- app/src/main/java/com/example/player_sample_project/authentication/LoginBaseActivity.kt
+++ app/src/main/java/com/example/player_sample_project/authentication/LoginBaseActivity.kt
@@ -1,5 +1,6 @@
 package com.example.player_sample_project.authentication
 
+import android.app.ActivityManager
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
@@ -10,6 +11,7 @@ import android.widget.TextView
 import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import com.example.player_sample_project.R
 import com.example.player_sample_project.activity.MainActivity
 import com.example.player_sample_project.app.AppController
@@ -68,6 +70,12 @@ class LoginBaseActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login_base)
+
+        // To change the default to preferred color of app description when goes to recent
+        val taskDescription = ActivityManager.TaskDescription(
+            getString(R.string.app_name), null, ContextCompat.getColor(this, R.color.dark_white))
+        setTaskDescription(taskDescription)
+
         setupViews()
         onclickSignInText()
         onclickSignUpText()
@@ -179,6 +187,10 @@ class LoginBaseActivity : AppCompatActivity() {
         orView = findViewById(R.id.or_view)
         googleLoginButton = findViewById(R.id.google_signIn_button)
         fbLogin = findViewById(R.id.facebook_signIn)
+
+        fbLogin.setOnClickListener(View.OnClickListener {
+            Toast.makeText(applicationContext, "Right now not implemented.Check with GoogleLogin or sign In here", Toast.LENGTH_LONG).show()
+        })
     }
 
     private fun onclickSignInText() {
@@ -190,6 +202,9 @@ class LoginBaseActivity : AppCompatActivity() {
 
                 signIn_text.visibility = View.GONE
                 signUp_text.visibility = View.VISIBLE
+
+                //initial focus
+                textView2.requestFocus()
             }
         })
     }
@@ -202,6 +217,9 @@ class LoginBaseActivity : AppCompatActivity() {
 
             signIn_text.visibility = View.VISIBLE
             signUp_text.visibility = View.GONE
+
+            //initial focus
+            textview1.requestFocus()
         })
     }
 
